c252da0469a6eb168515f3a162438eabd0352af8
nadjov <42535389+nadjov@users.noreply.github.com>
2018-09-11 16:06:30 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-
-	$(document).ready(function(){
+$(document).ready(function(){
 	  // Add smooth scrolling to all links
 	  $("a").on('click', function(event) {
 
@@ -22,7 +21,9 @@
 		  });
 		} // End if
 	  });
-	});
+});
+
+
 	$(document).ready(function() {
 			// Show or hide the sticky footer button
 			$(window).scroll(function() {
@@ -112,6 +113,33 @@ if($(window).innerWidth() <= 768) {
 			}, 1500 );
 		});
 }
+
+
+//menu
+
+var toggle = document.querySelector(".toggle");
+var menu = document.querySelector(".menu");
+var dropdown = document.querySelector(".dropdown");
+var dropdownStart = document.querySelector(".dropdownStart");
+
+toggle.addEventListener("click", function() {
+  menu.classList.toggle("showMenu");
+  /*
+  if (dropdown.offsetWidth === 0 && dropdown.offsetHeight === 0) {
+    dropdown.classList.remove("showDropdown")
+  }
+  */
+}, false);
+
+dropdownStart.addEventListener("click", function() {
+  dropdown.classList.toggle("showDropdown");
+}, false);
+
+
+//animation
+(function(){var a=document.querySelector('#el_gjK9Kx7UV'),b=a.querySelectorAll('style'),c=function(d){b.forEach(function(f){var g=f.textContent;g&amp;&amp;(f.textContent=g.replace(/transform-box:[^;\r\n]*/gi,'transform-box: '+d))})};c('initial'),window.requestAnimationFrame(function(){return c('fill-box')})})();
+
+(function(){var a=document.querySelector('#el_anHy2nW1GZ'),b=a.querySelectorAll('style'),c=function(d){b.forEach(function(f){var g=f.textContent;g&amp;&amp;(f.textContent=g.replace(/transform-box:[^;\r\n]*/gi,'transform-box: '+d))})};c('initial'),window.requestAnimationFrame(function(){return c('fill-box')})})();
 
 
 
